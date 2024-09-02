492295625711059d49ee53f4048550891f80fdc6
Seth <Seth.newbill@gmail.com>
2024-09-02 15:50:12 -0400

    about update

--- src/components/SettingsModal/AboutSection.vue
+++ src/components/SettingsModal/AboutSection.vue
@@ -2,9 +2,9 @@
   <div class="text-pretty px-3 text-center">
     <p>
       Bike rental stations are mapped by their latitude and longitude, with a grid
-      sized to the browser's dimensions. The y-axis maps to notes in the C major
-      pentatonic scale, and the x-axis represents octaves one through five. When a
-      bike is checked out or returned, the corresponding station's note plays.
+      sized to the browser's dimensions. The x-axis maps to notes in the C major
+      pentatonic scale, and the y-axis to octaves one through five. When a bike is
+      checked out or returned, the corresponding station's note plays.
     </p>
     <p>
       Zooming into different areas of the map changes the note and octave assigned to
