23984b80595e66be51048192f40427101c19f01c
Seth <Seth.newbill@gmail.com>
2024-09-02 10:52:17 -0400

    deploy updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 export default defineConfig({
   plugins: [
